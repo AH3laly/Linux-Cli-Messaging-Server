d7b26a1dc18c92b0406a80130e3562637388a67e
AH3laly <Ah3laly@gmail.com>
2024-12-08 09:54:00 +0800

    Implement headers.h

--- includes/headers.h
+++ includes/headers.h
@@ -1,1 +1,22 @@
-// Header files
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
+
+#include <netinet/in.h>
+
+#include <unistd.h> // Time (Sleep)
+#include <string.h>
+
+#include <termios.h>
+
+#include <stdbool.h>
+
+#include "definitions.h"
+#include "functions.h"
+#include "commands.h"
