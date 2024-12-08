f1b13b69c1e01fbd77a0b03e39b75eeaadb64fe0
AH3laly <Ah3laly@gmail.com>
2024-12-08 09:55:03 +0800

    Implement functions.h

--- includes/functions.h
+++ includes/functions.h
@@ -1,1 +1,30 @@
-// Functions Headers
+/**
+ * Ask user to input string
+*/
+void askUserInput(char *);
+
+/**
+ * Ask user to input Password
+*/
+void askPassword(char *);
+
+/**
+ * Push message to Console
+*/
+void printMessage(char *, int);
+
+/**
+ * Checks if text is empty
+*/
+bool isEmpty(char *);
+
+/**
+ * Empty text
+*/
+void clean(char *);
+bool equals(char *, char *);
+
+/**
+ * Concatinate username and password,
+*/
+void buildToken(char *, char *, char *);
