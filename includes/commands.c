eadeaa0307ecf7c1c9053afebfb9291fbd7311c0
AH3laly <Ah3laly@gmail.com>
2024-12-08 09:56:47 +0800

    Implement functions in commands.c

--- includes/commands.c
+++ includes/commands.c
@@ -1,1 +1,47 @@
-// Commands
+typedef struct {
+    char* name;
+    void (*ptr)(char*);
+    char* description;
+} command;
+
+command commands[] = {
+    {"help", &command_help, "Show available commands."},
+    {"reboot", &command_reboot, "Reboot the server."},
+    {"shutdown", &command_shutdown, "Shutdown the server."}
+};
+
+void execute(char *commandName, char *result){
+    printf("Executing Command: %s\n", commandName);
+    int length = sizeof(commands) / sizeof(commands[0]);
+    for(int i = 0; i < length; i++){
+        if(equals(commandName, commands[i].name)){
+            (commands[i].ptr)(result);
+            strcat(result, "\n");
+            return;
+        }
+    }
+    command_default(result);
+    strcat(result, "\n");
+}
+
+void command_default(char * result){
+    strcpy(result, "Command Not Found.");
+}
+
+void command_help(char * result){
+    int length = sizeof(commands) / sizeof(commands[0]);
+    for(int i = 0; i < length; i++){
+        strcat(result, "\n");
+        strcat(result, commands[i].name);
+        strcat(result, ": ");
+        strcat(result, commands[i].description);
+    }
+}
+
+void command_reboot(char * result){
+    strcpy(result, "Rebooting...");
+}
+
+void command_shutdown(char * result){
+    strcpy(result, "Shutting Down...");
+}
--- includes/commands.h
+++ includes/commands.h
@@ -1,1 +1,24 @@
-// Commands Headers
+/**
+ * Execute defined commands
+*/
+void execute(char *, char *);
+
+/**
+ * Fallback function in case command not found
+*/
+void command_default(char *);
+
+/**
+ * Show Available commands
+*/
+void command_help(char *);
+
+/**
+ * Reboot machine
+*/
+void command_reboot(char *);
+
+/**
+ * Shutdown machine
+*/
+void command_shutdown(char *);
